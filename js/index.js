a553878a7fe89ae1369be8ad9925a206ddb1c8ba
wangbiaozy <56565283@qq.com>
2017-07-05 02:35:41 +0800

    1

--- js/index.js
+++ js/index.js
@@ -7,21 +7,8 @@
 })(window);
 
 $(function () {
-    $('.clear').on('touchend','li',function () {
-        var $this = $(this);
-        //console.log($this.context.id);
-        if($this.context.id == 'baozhuang'){//报装进度查询跳转
-            window.location.href = '';
-        }else if($this.context.id == 'baozhang'){//宽带报障进度查询跳转
-            window.location.href = '';
-        }else if($this.context.id == 'xiaoqu'){//开通小区查询跳转
-            window.location.href = '';
-        }else if($this.context.id == 'xufei'){//宽带续费跳转
-            window.location.href = '';
-        }else if($this.context.id == 'xiugai'){//修改联系电话跳转
-            window.location.href = '';
-        }else if($this.context.id == 'paizhang'){//宽带自助排障跳转
-            window.location.href = '';
-        }
+    //立即申请
+    $('#btn').on('click',function () {
+        window.location.href = '';
     });
 });
